ec74b55fb7f73aa1e18234de2d4055f159264642
Kain Cortrecht <kaincortrecht@Kains-MacBook-Air.local>
2022-02-18 15:09:19 -0500

    all done i believe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
+  const [sW, setSw] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people`)
+      .then((res) => {
+        console.log(res.data);
+        setSw(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {sW.map((sW) => {
+        return <Character name={sW.name} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
